8aff4cc3d83e2afc9c0ed94058c4c0681ddc8155
Bartosz Pieńkowski <pienkowb@gmail.com>
2011-10-25 21:20:08 +0200

    Added a list function rendering recent posts.

---
+++ lists/recent.js
@@ -0,0 +1,15 @@
+function(head, req) {
+    start({"headers": {"Content-type": "text/html"}});
+
+    var mustache = require("lib/mustache");
+    var posts = [];
+
+    while(row = getRow()) {
+        posts.push({
+            "date": row.key,
+            "body": row.value
+        });
+    }
+
+    return mustache.to_html(this.templates.recent, {"posts": posts});
+}
